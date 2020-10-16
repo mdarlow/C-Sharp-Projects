49a23d0e7ab818c43f23813b0222908875f575da
mdarlow <mdarlow@gmail.com>
2020-10-16 10:41:26 -0700

    Push Loops & Assignment 183.

---
+++ Basic C-Sharp Projects - The Tech Academy/ConsoleAppAssignment_183/ConsoleAppAssignment_183/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment_183
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the best dessert in the world?");
+            string guess = Convert.ToString(Console.ReadLine());
+            bool isGuessed = guess == "cheesecake";
+
+            do
+            {
+                switch (guess)
+                {
+                    case "cake":
+                        Console.WriteLine("Good guess, but ultimately incorrect.\nPlease try again.");
+                        guess = Convert.ToString(Console.ReadLine());
+                        break;
+                    case "cheesecake":
+                        Console.WriteLine("Cheesecake is correct!\nIt's the inherent best dessert ever!\nCongrats for understanding this universal truth!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("That is incorrect.\nPlease try again.");
+                        guess = Convert.ToString(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+            Console.ReadLine();
+        }
+    }
+}
