4ffe775de515d2039d59d2168cc98eafce202192
mdarlow <mdarlow@gmail.com>
2020-10-16 13:17:16 -0700

    Push Assignment 186.

---
+++ Basic C-Sharp Projects - The Tech Academy/Assignment_186/Assignment_186/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_186
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Michael";
+            string verb = " said, ";
+            string quote = "\"Hello!\"";
+
+            name = name.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Michael. ");
+            sb.Append("My last name is Darlow. ");
+            sb.Append("That's like \"Barlow,\" but even better.");
+
+            Console.WriteLine(name + verb + quote);
+            Console.WriteLine("\n" + sb);
+            Console.ReadLine();
+        }
+    }
+}
