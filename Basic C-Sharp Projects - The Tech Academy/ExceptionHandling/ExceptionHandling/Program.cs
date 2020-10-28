fea486c136b6537af0b56ddf39df8fbb2726f530
mdarlow <mdarlow@gmail.com>
2020-10-27 18:13:29 -0700

    Push Exception Handling assignments.

---
+++ Basic C-Sharp Projects - The Tech Academy/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Pick a number.");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number.");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Divinding the two...");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+        }
+        // Catching a FormatException and assigning it to variable ex:
+        catch (FormatException ex)
+        {
+            // Message of the variable ex error:
+            // Console.WriteLine(ex.Message);
+
+            // My own error message:
+            Console.WriteLine("Please enter an integer.");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero.");
+        }
+        // If it's any other exception:
+        catch (Exception ex)
+        {
+            // Display C#'s error message:
+            Console.WriteLine(ex.Message);
+        }
+        // Runs no matter what:
+        finally
+        {
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic C-Sharp Projects - The Tech Academy/ExceptionHandlingAssignment_214/ExceptionHandlingAssignment_214/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> integers = new List<int>() { 0, 12, 23, 34, 45, 56, 67, 78, 89, 910, 1011 };
+        Console.WriteLine("Provide a number to divide each number in the list by.");
+
+
+        try
+        {
+            int usrInput = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < integers.Count; i++)
+            {
+                Console.WriteLine(integers[i] / usrInput);
+            }
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero.");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please enter an integer.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Program has emerged from the try/catch block.");
+            Console.ReadLine();
+        }
+    }
+}
