4349663efdb96fd2d71a6d2c353d813705f02105
mdarlow <mdarlow@gmail.com>
2020-10-29 16:38:17 -0700

    Push Method Overloading Assignment.

---
+++ Basic C-Sharp Projects - The Tech Academy/MethodOverloadingAssignment_226/MethodOverloadingAssignment_226/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingAssignment_226
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minuend1 = 10;
+            double minuend2 = 10.258;
+            string minuend3 = "10";
+            Console.WriteLine("{0} - 3 = {1}", minuend1, MathOperation.SubtractByThree(minuend1));
+            Console.WriteLine("{0} - 3 = {1}", minuend2, MathOperation.SubtractByThree(minuend2));
+            Console.WriteLine("{0} - 3 = {1}", minuend3, MathOperation.SubtractByThree(minuend3));
+            Console.ReadLine();
+        }
+    }
+    class MathOperation
+    {
+        public static int SubtractByThree(int minuend1)
+        {
+            int difference = minuend1 - 3;
+            return difference;
+        }
+        public static double SubtractByThree(double minuend2)
+        {
+            int convertedMinuend2 = Convert.ToInt32(minuend2);
+            int difference = convertedMinuend2 - 3;
+            return difference;
+        }
+        public static string SubtractByThree(string minuend3)
+        {
+            int convertedMinuend3 = Convert.ToInt32(minuend3);
+            int difference = convertedMinuend3 - 3;
+            return difference;
+        }
+    }
+}
