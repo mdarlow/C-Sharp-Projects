e29ecdac59b13cc977dc36a0ccbee11d049ef618
mdarlow <mdarlow@gmail.com>
2020-10-29 12:58:43 -0700

    Push Methods Assignment.

---
+++ Basic C-Sharp Projects - The Tech Academy/MathOperationMethodsAssignment_223/MathOperationMethodsAssignment_223/MathOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperationMethodsAssignment_223
+{
+    class MathOperations
+    {
+        public static int DivideByThree(int usrInput)
+        {
+            return usrInput / 3;
+        }
+        public static int MultiplyByThree(int usrInput)
+        {
+            return usrInput * 3;
+        }
+        public static int AddThree(int usrInput)
+        {
+            return usrInput + 3;
+        }
+    }
+}
---
+++ Basic C-Sharp Projects - The Tech Academy/MathOperationMethodsAssignment_223/MathOperationMethodsAssignment_223/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperationMethodsAssignment_223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a number for the math operations to be performed on.");
+            int usrInput = Convert.ToInt32(Console.ReadLine());
+
+            // Method #1
+            // Division class ==> DivideByThree method
+            Console.WriteLine("{0} \u00F7 3 = {1}.", usrInput, MathOperations.DivideByThree(usrInput));
+            Console.ReadLine();
+
+            // Method #2
+            // Multiplication class ==> MultiplyByThree method
+            Console.WriteLine("{0} X 3 = {1}.", usrInput, MathOperations.MultiplyByThree(usrInput));
+            Console.ReadLine();
+
+            // Method #3
+            // Addition class ==> AddThree method
+            Console.WriteLine("{0} + 3 = {1}.", usrInput, MathOperations.AddThree(usrInput));
+            Console.ReadLine();
+        }
+    }
+}
