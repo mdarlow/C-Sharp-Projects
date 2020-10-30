7baebeeaa1800a7efc2b84d98cc8ecc6be6d3bce
mdarlow <mdarlow@gmail.com>
2020-10-30 11:26:14 -0700

    Update OptionalParameterAssignment.

--- Basic C-Sharp Projects - The Tech Academy/OptionalParameterAssignment_227/OptionalParameterAssignment_227/Program.cs
+++ Basic C-Sharp Projects - The Tech Academy/OptionalParameterAssignment_227/OptionalParameterAssignment_227/Program.cs
@@ -12,15 +12,25 @@ namespace OptionalParameterAssignment_227
         {
             Console.WriteLine("For exponentiation, please input one base number and then one exponent. Exponent will default to \"2\" if left blank.");
             double inputBase = Convert.ToInt32(Console.ReadLine());
-            double inputExponent = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("{0} ^ {1} = {2}", inputBase, inputExponent, MathOperation.Exponentiation(inputBase, inputExponent));
+            string inputExponent = Console.ReadLine();
+
+            if (inputExponent == "")
+            {
+                Console.WriteLine("{0} ^ {1} = {2}", inputBase, 2, MathOperation.Exponentiation(inputBase));
+            }
+            else
+            {
+                double convertedExponent = Convert.ToDouble(inputExponent);
+                Console.WriteLine("{0} ^ {1} = {2}", inputBase, inputExponent, MathOperation.Exponentiation(inputBase, convertedExponent));
+            }
             Console.ReadLine();
+
         }
         class MathOperation
         {
-            public static int Exponentiation(double inputBase, double inputExponent = 2)
+            public static int Exponentiation(double inputBase, double convertedExponent = 2)
             {
-                int power = Convert.ToInt32(Math.Pow(inputBase, inputExponent));
+                int power = Convert.ToInt32(Math.Pow(inputBase, convertedExponent));
                 return power;
             }
         }
