1fdf0f5e2dc4c82a27a4add1eb1ce47ef21f4cbe
mdarlow <mdarlow@gmail.com>
2020-10-29 17:28:20 -0700

    Update Method Overloading Assignment.

--- Basic C-Sharp Projects - The Tech Academy/MethodOverloadingAssignment_226/MethodOverloadingAssignment_226/Program.cs
+++ Basic C-Sharp Projects - The Tech Academy/MethodOverloadingAssignment_226/MethodOverloadingAssignment_226/Program.cs
@@ -13,12 +13,17 @@ namespace MethodOverloadingAssignment_226
             int minuend1 = 10;
             double minuend2 = 10.258;
             string minuend3 = "10";
+            Console.WriteLine("Integer to integer:");
             Console.WriteLine("{0} - 3 = {1}", minuend1, MathOperation.SubtractByThree(minuend1));
+            Console.WriteLine("\nDecimal to integer:");
             Console.WriteLine("{0} - 3 = {1}", minuend2, MathOperation.SubtractByThree(minuend2));
+            Console.WriteLine("\nString to integer:");
             Console.WriteLine("{0} - 3 = {1}", minuend3, MathOperation.SubtractByThree(minuend3));
             Console.ReadLine();
         }
     }
+
+
     class MathOperation
     {
         public static int SubtractByThree(int minuend1)
@@ -26,13 +31,15 @@ namespace MethodOverloadingAssignment_226
             int difference = minuend1 - 3;
             return difference;
         }
-        public static double SubtractByThree(double minuend2)
+
+        public static int SubtractByThree(double minuend2)
         {
             int convertedMinuend2 = Convert.ToInt32(minuend2);
             int difference = convertedMinuend2 - 3;
             return difference;
         }
-        public static string SubtractByThree(string minuend3)
+
+        public static int SubtractByThree(string minuend3)
         {
             int convertedMinuend3 = Convert.ToInt32(minuend3);
             int difference = convertedMinuend3 - 3;
