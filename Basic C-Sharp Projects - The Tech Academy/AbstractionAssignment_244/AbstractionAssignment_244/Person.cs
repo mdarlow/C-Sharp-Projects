a6f4632a2765f7f7db71fb7bc4dad85c57389395
mdarlow <mdarlow@gmail.com>
2020-11-05 11:36:06 -0800

    Push AbstractionAssignment.

---
+++ Basic C-Sharp Projects - The Tech Academy/AbstractionAssignment_244/AbstractionAssignment_244/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionAssignment_244
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public virtual void SayName()
+        {
+            Console.WriteLine("{0} {1}", firstName, lastName);
+        }
+    }
+}
