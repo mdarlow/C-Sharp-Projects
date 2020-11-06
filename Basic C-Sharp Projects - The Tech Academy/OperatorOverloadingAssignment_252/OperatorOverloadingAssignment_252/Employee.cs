37a1387b9a27c226ee65a7a718804bfd6e7761c4
mdarlow <mdarlow@gmail.com>
2020-11-05 18:16:46 -0800

    Push OperatorOverloadingAssignment.

---
+++ Basic C-Sharp Projects - The Tech Academy/OperatorOverloadingAssignment_252/OperatorOverloadingAssignment_252/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingAssignment_252
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id
+        {
+            get; set;
+        }
+        public override void SayName()
+        {
+            Console.WriteLine("{0} {1}", firstName, lastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Press enter to quit.");
+        }
+        
+          //////////////////////////////
+         /// Overload "==" operator ///
+        //////////////////////////////
+        public static Employee operator== (Employee employee1, Employee employee2)
+        {
+            return (employee1 == employee2);
+        }
+        public static Employee operator!= (Employee employee1, Employee employee2)
+        {
+            return (employee1 != employee2);
+        }
+    }
+}
---
+++ Basic C-Sharp Projects - The Tech Academy/OperatorOverloadingAssignment_252/OperatorOverloadingAssignment_252/Program.cs
@@ -0,0 +1,30 @@
+﻿// Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingAssignment_252
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              /////////////////////////////
+             /// Instances of Employee ///
+            /////////////////////////////
+            Employee employee1 = new Employee();
+            employee1.Id = 1;
+            Employee employee2 = new Employee();
+            employee2.Id = 2;
+
+              /////////////////////////////
+             /// Compare Id properties ///
+            /////////////////////////////
+            Console.WriteLine(employee1.Id == employee2.Id);
+            Console.ReadLine();
+        }
+    }
+}
