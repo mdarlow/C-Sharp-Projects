0f0f4f63698a612cfe1d2f9592fbb6972bd7c34f
mdarlow <mdarlow@gmail.com>
2020-11-06 13:18:07 -0800

    Push GenericsAssignment.

---
+++ Basic C-Sharp Projects - The Tech Academy/GenericsAssignment_255/GenericsAssignment_255/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment_255
+{
+    public class Employee<T> : Person, IQuittable
+    {
+          //////////////////////////////
+         // Employee class property: //
+        //////////////////////////////
+        public List<T> things { get; set; }
+
+        
+
+        public int Id
+        {
+            get; set;
+        }
+        public override void SayName()
+        {
+            Console.WriteLine("{0} {1}", firstName, lastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Press enter to quit.");
+        }
+
+        //public static bool operator ==(Employee employee1, Employee employee2)
+        //{
+        //    return employee1.Id == employee2.Id;
+        //}
+        //public static bool operator !=(Employee employee1, Employee employee2)
+        //{
+        //    return employee1.Id == employee2.Id;
+        //}
+    }
+}
---
+++ Basic C-Sharp Projects - The Tech Academy/GenericsAssignment_255/GenericsAssignment_255/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment_255
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              //////////////////////////////////////////////////
+             // Instantiate Employee object with type string //
+            //////////////////////////////////////////////////
+            Employee<string> employeeStringList = new Employee<string>();
+            employeeStringList.Add("asdf");
+            employeeStringList.Add("ghjk");
+            employeeStringList.Add("l;\'");
+
+              ///////////////////////////////////////////////
+             // Instantiate Employee object with type int //
+            ///////////////////////////////////////////////
+            Employee<int> employeeIntList = new Employee<int>();
+            employeeIntList.Add(1);
+            employeeIntList.Add(2);
+            employeeIntList.Add(3);
+
+            foreach (Employee<string> thing in Employee<string>.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+
+
+
+            //Employee employee1 = new Employee();
+            //employee1.Id = 1;
+            //Employee employee2 = new Employee();
+            //employee2.Id = 2;
+            
+            //Console.WriteLine(employee1 == employee2);
+            //Console.ReadLine();
+        }
+    }
+}
