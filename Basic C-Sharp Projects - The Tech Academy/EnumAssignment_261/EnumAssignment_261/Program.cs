1a6e0c471cc71c2c6ba55ee747bc4f3b75e65c83
mdarlow <mdarlow@gmail.com>
2020-11-06 17:44:32 -0800

    Push EnumAssignment.

---
+++ Basic C-Sharp Projects - The Tech Academy/EnumAssignment_261/EnumAssignment_261/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumAssignment_261
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week.");
+                DaysOfWeek currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), Console.ReadLine());
+                Console.WriteLine("Happy {0} to you!", currentDay);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+
+        }
+
+          //////////
+         // Enum //
+        //////////
+        public enum DaysOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
