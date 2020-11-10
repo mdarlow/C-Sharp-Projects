6647fa91c54e8063e4af4db3950222c27663c027
mdarlow <mdarlow@gmail.com>
2020-11-10 10:49:28 -0800

    Push StructsAssignment.

---
+++ Basic C-Sharp Projects - The Tech Academy/StructsAssignment_267/StructsAssignment_267/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssignment_267
+{
+    class Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Basic C-Sharp Projects - The Tech Academy/StructsAssignment_267/StructsAssignment_267/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssignment_267
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number() { Amount = 2.5m };
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
