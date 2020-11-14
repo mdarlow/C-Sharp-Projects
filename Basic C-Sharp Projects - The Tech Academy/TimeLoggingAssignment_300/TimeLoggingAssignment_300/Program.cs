eabd91b880fbe28568c097dfa94a9b3db2739adc
mdarlow <mdarlow@gmail.com>
2020-11-13 16:27:02 -0800

    Push TimeLoggingAssignment.

---
+++ Basic C-Sharp Projects - The Tech Academy/TimeLoggingAssignment_300/TimeLoggingAssignment_300/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TimeLoggingAssignment_300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the current date and time to the console:
+            DateTime currentDateAndTime = DateTime.Now;
+            Console.WriteLine(currentDateAndTime);
+
+            // Ask the user for a number:
+            Console.WriteLine("Please enter a number:");
+            int aNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Print to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+            Console.WriteLine(currentDateAndTime.AddHours(aNumber));
+            Console.ReadLine();
+
+        }
+    }
+}
