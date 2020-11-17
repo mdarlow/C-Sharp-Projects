c62d289099d84209bf4cacb9186a4178cdc9fcfb
mdarlow <mdarlow@gmail.com>
2020-11-17 10:27:58 -0800

    Push AdditionalFeaturesAssignment.

---
+++ Basic C-Sharp Projects - The Tech Academy/AdditionalFeaturesAssignment_310/AdditionalFeaturesAssignment_310/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssignment_310
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string constVar = "This is a \"constant variable\"";
+
+            var keywordVar = "This is an implicitly typed variable";
+
+            Console.WriteLine("{0}.\n{1}.", constVar, keywordVar);
+
+            Person newPerson = new Person("Bob");
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+    internal class Person
+    {
+        private string favColor;
+        private string Name;
+
+        public Person(string name) : this(name, "blue")
+        {
+        }
+        public Person(string name, string color)
+        {
+            favColor = color;
+            Name = name;
+            Console.WriteLine("{0}\'s favorite color is {1}.", Name, favColor);
+        }
+    }
+}
