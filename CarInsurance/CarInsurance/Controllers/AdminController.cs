2d51d6fd520ebde1b3fa021a4b072071b8bd1856
mdarlow <mdarlow@gmail.com>
2020-12-02 21:40:56 -0800

    Push CarInsurance.
    Admin page shows quotes, first name, last name, and email address.

--- CarInsurance/CarInsurance/Controllers/AdminController.cs
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -1,6 +1,4 @@
-﻿using CarInsurance.Models;
-using CarInsurance.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -8,61 +6,122 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using CarInsurance.Models;
 
 namespace CarInsurance.Controllers
 {
     public class AdminController : Controller
     {
+        private InsuranceEntities db = new InsuranceEntities();
+
         // GET: Admin
         public ActionResult Index()
         {
-            using (InsuranceEntities db = new InsuranceEntities())
+            return View(db.Insurees.ToList());
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
             {
-                var insurees = (from c in db.Insurees
-                                select c).ToList();
-                var insureeVms = new List<InsureeVm>();
-                foreach (var insuree in insurees)
-                {
-                    var insureeVm = new InsureeVm();
-                    insureeVm.Id = insuree.Id;
-                    insureeVm.FirstName = insuree.FirstName;
-                    insureeVm.LastName = insuree.LastName;
-                    insureeVm.EmailAddress = insuree.EmailAddress;
-                    insureeVm.DateOfBirth = insuree.DateOfBirth;
-                    insureeVm.CarYear = insuree.CarYear;
-                    insureeVm.CarModel = insuree.CarModel;
-                    insureeVm.DUI = insuree.DUI;
-                    insureeVm.SpeedingTickets = insuree.SpeedingTickets;
-                    insureeVm.CoverageType = insuree.CoverageType;
-                    insureeVm.Quote = insuree.Quote;
-                }
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
             return View();
         }
-        public ActionResult AdminIndex()
+
+        // POST: Admin/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
         {
-            using (InsuranceEntities db = new InsuranceEntities())
+            if (ModelState.IsValid)
             {
-                var insurees = (from c in db.Insurees
-                                select c).ToList();
-                var insureeVms = new List<InsureeVm>();
-                foreach (var insuree in insurees)
-                {
-                    var insureeVm = new InsureeVm();
-                    insureeVm.Id = insuree.Id;
-                    insureeVm.FirstName = insuree.FirstName;
-                    insureeVm.LastName = insuree.LastName;
-                    insureeVm.EmailAddress = insuree.EmailAddress;
-                    insureeVm.DateOfBirth = insuree.DateOfBirth;
-                    insureeVm.CarYear = insuree.CarYear;
-                    insureeVm.CarModel = insuree.CarModel;
-                    insureeVm.DUI = insuree.DUI;
-                    insureeVm.SpeedingTickets = insuree.SpeedingTickets;
-                    insureeVm.CoverageType = insuree.CoverageType;
-                    insureeVm.Quote = insuree.Quote;
-                }
+                db.Insurees.Add(insuree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
             }
-            return View();
+
+            return View(insuree);
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(insuree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
